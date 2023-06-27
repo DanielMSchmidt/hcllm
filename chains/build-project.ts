14ef803933fd08f8b7fdb39734a603f32413812d
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-06-27 12:03:58 +0200

    chore: try modules

--- chains/build-project.ts
+++ chains/build-project.ts
@@ -8,13 +8,15 @@ export default defineChain({
       type: "string",
     },
     expectations: { type: "string" },
+    inputs: { type: "string" },
+    outputs: { type: "string" },
   },
   setup({ params, step }) {
-    const { problemStatement, expectations } = params;
+    const { problemStatement, expectations, inputs, outputs } = params;
 
     const { answer } = step("prompt_completion", {
-      system_prompt: `You are a cloud architect working in AWS. You have been tasked with designing a solution to a problem. The problem is: ${problemStatement}. The solution must meet the following requirements: ${expectations}. All Terraform variables except for the AWS credentials should come with sensible defaults. If a non-HCL file is required for the solution, it should be added to the JSON.`,
-      prompt: `You are leading a team of developers to build a solution to this problem. Write Terraform configuration in HCL to solve the problem. Also list any programming jobs needed to solve the problem. The format of the solution is JSON the fields 'code' and 'tasks'. The 'code' field is an object where the key is the relative file path and the value being the contents of the file. The code field should be used for the Terraform code and if all files were put into file system they should be runnable in the current directory. The 'tasks' field is an array of objects where each object has a 'folder' and 'prompt' field. The 'directory' field is the name of the directory where the HCL code expects the application (e.g. frontend or backend) to be and the 'prompt' field is a description of the content of the directory containing all the context needed for a LLM to generate the content. The tasks field should be used as a list of tasks for the developers to complete. If the service should be reachable from the public internet include a top level field 'url_output' with the name of the output containing the address of the load balancer to the response.`,
+      system_prompt: `You are a cloud architect working in AWS. You are diligent and always include a README file in your projects. You use Terraform and write it as HCL. You produce well-documented Terraform Modules with just the inputs and outputs specified.`,
+      prompt: `You have been tasked with designing a Terraform Module to solve this problem: ${problemStatement}. The solution must meet the following requirements: ${expectations}. The Terraform Module takes only these inputs, the parentheses denote the description: ${inputs}. These are the outputs for the module in the same format: ${outputs}. Give the solution in JSON format. The solution should be an object where keys are the relative paths of the files to be created and the value is the content of the file in HCL. The solution should be a valid Terraform Module.`,
       validators: [
         {
           _oneof_type_: "is_json",
